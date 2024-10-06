eaffc9fe28f4aee5cd97e8d449f0f44152610bb5
artur artur <arturk102007@gmail.com>
2024-10-06 21:35:54 +0300

    Массивы

---
+++ main.js
@@ -0,0 +1,15 @@
+// let questions = new Array("Вам нравится быть фронтэнд разработчиком?", "Вам нравится быть бэкэнд разработчиком?", "Вы работаете с Анатолием?", "Код написан на языке программирования JavaScript?");
+// let answers = ["Да", "Да", "Нет", "Да"];
+
+// let score = 0;
+
+// for (let i = 0; i < questions.length; i++) 
+// {
+//     let answer = prompt(questions[i] + " (Да / Нет)");
+//     if (answer == answers[i]) 
+//     {
+//         score ++;
+//     }
+// }
+
+// alert("Ваш результат: " + score + " из " + questions.length);
